5c7598843f6e65b967a1f797cc619a00491e655c
mospky <15t8013n@vc.ibaraki.ac.jp>
2018-09-21 17:30:32 +0900

    ref

--- train.py
+++ train.py
@@ -56,20 +56,20 @@ print('y_train[0]', y_train[0])
 
 model = Sequential()
 
-#first layer
+# first layer
 model.add(Conv2D(32, kernel_size=(3,3),
             activation='relu',
             input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Dropout(0.2))
 
-#second layer
+# second layer
 model.add(ZeroPadding2D(padding=(1, 1)))
 model.add(Conv2D(96, kernel_size=(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Dropout(0.2))
 
-#third layer
+# third layer
 model.add(ZeroPadding2D(padding=(1, 1)))
 model.add(Conv2D(96, kernel_size=(3,3)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
